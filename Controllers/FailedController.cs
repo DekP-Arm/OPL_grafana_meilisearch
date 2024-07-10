b9ec0735787262b455d5ca0386d9534513aaa946
belliorgabxl <64011224@kmitl.ac.th>
2024-07-10 09:33:39 +0700

    edite again (status code)

--- Controllers/FailedController.cs
+++ Controllers/FailedController.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using OPL_grafana_meilisearch.DTOs;
 using OPL_grafana_meilisearch.src.Core.Interface;
-using ILogger = Serilog.ILogger;
+using Microsoft.Extensions.Logging;
+using Meilisearch;
+
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 
 namespace OPL_grafana_meilisearch.Controllers
 {
@@ -10,13 +15,25 @@ namespace OPL_grafana_meilisearch.Controllers
     public class FailedController : Controller
     {
         private readonly IFailedService _failedService;
-        private readonly ILogger _logger;
+        private readonly ILogger<FailedController> _logger;
+        private readonly MeilisearchClient _meilisearch;
 
-        public FailedController(IFailedService failedService, ILogger logger)
+        private static readonly ActivitySource activitySource = new ActivitySource("APITracing");
+
+        // Extracts context in HTTP headers and injects context into HTTP headers.
+        private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
+
+        private readonly string meilisearchHost ="";
+
+        private readonly string meilisearchApiKey= "";
+
+        public FailedController(IFailedService failedService, ILogger<FailedController> logger, IConfiguration configuration)
         {
             _failedService = failedService;
             _logger = logger;
-
+            meilisearchHost = configuration.GetValue<string>("MeilisearchClient:Host");
+            meilisearchApiKey = configuration.GetValue<string>("MeilisearchClient:ApiKey");
+            _meilisearch = new MeilisearchClient(meilisearchHost, meilisearchApiKey);
         }
 
         [HttpGet("GetAllFailed")]
@@ -25,17 +42,59 @@ namespace OPL_grafana_meilisearch.Controllers
             var response = new BaseHttpResponse<List<FailedDto>>();
             try
             {
-                var data = await _failedService.GetAllFailedAsync();
-                response.SetSuccess(data, "Success", "200");
-                return Ok(response);
+                using (var activity = activitySource.StartActivity("FailedController-GetAllFailedAsync",ActivityKind.Server))
+                {
+                    activity?.SetTag("component", "FailedController");
+                    activity?.SetTag("http.method", "GET");
+                    activity?.SetTag("http.url", "/api/failed/GetAllFailed");
+                    
+                    using (var failServiceActivity = activitySource.StartActivity("Call FailService", ActivityKind.Internal))
+                    {
+                        // Tag the activity with the name of the service and the method being called
+                        failServiceActivity?.SetTag("internal.service.name", "FailedService");
+                        failServiceActivity?.SetTag("internal.service.method", "GetAllUserAsync");
+
+                        // Call the service
+                        var data = await _failedService.GetAllFailedAsync();
+                        response.Data = data;
+                    }
+                    return Ok(response);
+
+                }
             }
             catch (Exception ex)
             {
-                ErrorData err = new ErrorData();
-                err.Code = "500 : internal server error";
-                err.Message = "500 : Error getting Faileds";
-                _logger.Error(ex, "Error getting All Faileds");
-                return StatusCode(500, err);
+                using (var activity = activitySource.StartActivity("FailedController-GetAllFailedAsync-Error",ActivityKind.Server))
+                {
+                    activity.SetTag("error", true);
+                    activity.SetTag("error.message", ex.Message);
+                    activity.SetTag("error.stacktrace", ex.StackTrace);
+                    var err = new ErrorData
+                    {
+                        Code = "1-GetAllUsers",
+                        Message = "Error getting Users"
+                    };
+                    _logger.LogError(ex, "Error getting users");
+
+                    using (var meilisearchActivity = activitySource.StartActivity("Send Data to Meilisearch", ActivityKind.Client))
+                        {
+                            DateTime currentDateTime = DateTime.Now;
+                            var formattedDateTime = currentDateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+                            var error_log = new[]
+                            {
+                                new ErrorLogMeilisearchDto
+                                {
+                                    CodeId = Guid.NewGuid().ToString("N"),
+                                    Api = "1-GetAllFaileds",
+                                    Message = "Error getting All Faileds",
+                                    dateTime = formattedDateTime,
+                                }
+                            };
+                            var index = _meilisearch.Index("Error_log");
+                            await index.AddDocumentsAsync(error_log);
+                        }
+                    return StatusCode(StatusCodes.Status500InternalServerError, err);
+                }
             }
         }
         [HttpPost("AddFailed")]
@@ -44,20 +103,49 @@ namespace OPL_grafana_meilisearch.Controllers
             var response = new BaseHttpResponse<List<FailedDto>>();
             try
             {
+                using (var activity = activitySource.StartActivity("FailedController-AddFailedAsync",ActivityKind.Server))
+                {
+                    activity?.SetTag("component", "FailedController");
+                    activity?.SetTag("http.method", "POST");
+                    activity?.SetTag("http.url", "/api/failed/AddFailedAsync");
 
-                var data = await _failedService.AddFailedAsync(username, password);
-                response.SetSuccess(data, "Success", "200");
-                return Ok(response);
+
+                    using (var userServiceActivity = activitySource.StartActivity("Call FailedService", ActivityKind.Internal))
+                    {
+                        // Tag the activity with the name of the service and the method being called
+                        userServiceActivity?.SetTag("internal.service.name", "FailedService");
+                        userServiceActivity?.SetTag("internal.service.method", "AddFailedAsync");
+
+                        var data = await _failedService.AddFailedAsync(username,password);
+                    }
+                    return Ok(response);
+
+                }
             }
             catch (Exception ex)
             {
-                var err = new ErrorData
+                using (var activity = activitySource.StartActivity("FailedController-AddUser-Error",ActivityKind.Server))
                 {
-                    Code = "Errors 500 : Internal Server Errors",
-                    Message = "Error adding faileds"
-                };
-                _logger.Error(ex, "500 : Internal Server Errors");
-                return StatusCode(500, err);
+                    activity.SetTag("error", true);
+                    activity.SetTag("error.message", ex.Message);
+                    activity.SetTag("error.stacktrace", ex.StackTrace);
+
+                    var err = new ErrorData
+                    {
+                        Code = "1-AddUser",
+                        Message = "Error adding Users"
+                    };
+
+                    _logger.LogError(ex, "Error adding users");
+
+                    using (var meilisearchActivity = activitySource.StartActivity("Send Data to Meilisearch", ActivityKind.Client))
+                        {
+
+                        }
+
+                    
+                    return StatusCode(StatusCodes.Status500InternalServerError, err);
+                }
             }
         }
 
